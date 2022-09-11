a63e8ecc066bf49cd1249ed3237994627b420845
Vasishth Gupta <vasishthemail@gmail.com>
2022-09-11 18:46:16 +0530

    update

---
+++ resources/js/main.js
@@ -0,0 +1,47 @@
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".nav");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+
+
+// cursor
+const $bigBall = document.querySelector('.cursor__ball--big');
+const $smallBall = document.querySelector('.cursor__ball--small');
+const $hoverables = document.querySelectorAll('.hoverable');
+
+// Listeners
+document.body.addEventListener('mousemove', onMouseMove);
+for (let i = 0; i < $hoverables.length; i++) {
+  $hoverables[i].addEventListener('mouseenter', onMouseHover);
+  $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+}
+
+
+// Move the cursor
+function onMouseMove(e) {
+  TweenMax.to($bigBall, .4, {
+    x: e.clientX - 15,
+    y: e.clientY - 15
+    
+  })
+  TweenMax.to($smallBall, .1, {
+    x: e.clientX - 5,
+    y: e.clientY - 5
+  })
+}
+
+// Hover an element
+function onMouseHover() {
+  TweenMax.to($bigBall, .3, {
+    scale: 3
+  })
+}
+function onMouseHoverOut() {
+  TweenMax.to($bigBall, .3, {
+    scale: 1
+  })
+}
+
+console.log(Math.random()*100)
